2cd37553a25a646cffadb47bc268a583f7d183c9
fahimmehemmod2005 <mdfahimmehemmud@gmail.com>
2026-02-01 09:46:34 +0600

    app done

--- lib/core/constant/app_color.dart
+++ lib/core/constant/app_color.dart
@@ -9,5 +9,9 @@ class AppColor {
   static const Color deepPrimaryColor = Color(0xff24019C);
   static const Color grayBlack = Color(0xff4A4C56);
   static const Color normal = Color(0xffF6F8FA);
-  static const Gradient primaryGradient = LinearGradient(begin: Alignment.topCenter,end: Alignment.bottomCenter,colors: [Color(0xff9785F5),Color(0xff24019C)]);
+  static const Gradient primaryGradient = LinearGradient(
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+    colors: [Color(0xff9785F5), Color(0xff24019C)],
+  );
 }
